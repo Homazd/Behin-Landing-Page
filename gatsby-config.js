8ccd8f387bd74fdbda592d0c3a0324786c50418e
homazd <homa.zohdi@hotmail.com>
2023-06-11 13:34:41 +0330

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Behin Landing Page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
