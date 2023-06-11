2caccc44ec6a4f1f81401a05b4098c7d1a6d8f3c
homazd <homa.zohdi@hotmail.com>
2023-06-11 16:51:33 +0330

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
         <div className='flex h-8 bg-violet-800 text-slate-50 text-center'>
         <span className='font-bold text-sm my-0.5 mx-auto'>Impossible Just Takes Longer To Design</span>
        
-            <div className='w-24 text-slate-50 text-sm my-0.5'>Company</div>
+            <div className='w-24 text-slate-50 text-sm my-0.5'>Company...</div>
        
     </div>
     )
