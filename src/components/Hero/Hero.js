db9e997691f6fcee211049fd2c077507a86e08fe
homazd <homa.zohdi@hotmail.com>
2023-06-26 09:45:09 +0330

    feat: change Hero background image,add WhyChooseUS

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,23 +1,23 @@
 import * as React from "react";
-import wave from "../../images/wave.png";
+import bg1 from "../../images/bg1.png";
 
 function Hero() {
   return (
     <div
-      className="pt-[56px] bg-center bg-cover desktop:w-[1170px] desktop:mx-auto desktop:rounded-lg"
-      // style={{ backgroundImage: `url(${wave})` }}
+      className="pt-[70px] laptop:pt-[156px] bg-center bg-cover desktop:w-screen h-[400px] tablet:h-[350px] laptop:h-[500px] desktop:mx-auto desktop:rounded-lg"
+      style={{ backgroundImage: `url(${bg1})` }}
     >
       <div className="px-[15px] tablet:text-center">
-        <p className="font-bold text-center text-[26px] tablet:text-[38px] font-sans text-blue-900 mb-[24px]">
+        <p className="font-bold text-center text-[26px] tablet:text-[38px] font-sans text-white mb-[24px]">
           BBDH
         </p>
         <div className="mb-[24px]">
-          <p className="text-[14px] tablet:text-[18px] laptop:text-[20px] pt-[20px] font-bold text-gray-500">
+          <p className="text-[14px] tablet:text-[18px] laptop:text-[20px] pt-[20px] text-gray-300">
             Innovative Software and Hardware solutions for telecommunications
           </p>
         </div>
-        <div className="tablet:flex tablet:justify-center">
-          <div className="bg-blue-900 h-[50px] text-white text-md hover:shadow-xl mb-[15px] rounded-lg font-semibold tablet:w-[225px] tablet:h-[50px] tablet:mx-[10px] tablet:my-0">
+        <div className="tablet:flex tablet:justify-center laptop:mt-[50px]">
+          <div className="bg-blue-700 h-[50px] text-white text-md hover:shadow-xl mb-[15px] rounded-lg font-semibold tablet:w-[225px] tablet:h-[50px] tablet:mx-[10px] tablet:my-0">
             <p className="text-[16px] py-[15px] px-[20px] b-1 text-center">
               Products
             </p>
---
+++ src/components/Why/WhyChooseUs.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+function WhyChooseUs() {
+
+}
+export default WhyChooseUs;
